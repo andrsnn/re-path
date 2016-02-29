db3c8a3a3cc241b78b08435609c4af2691fe9d6f
andrsnn <aidan.anderson.359@my.csun.edu>
2016-02-28 16:04:48 -0800

    added memoise and prompt for list

--- cli.js
+++ cli.js
@@ -1,45 +1,73 @@
+'use strict';
 //node main.js <pathToRoot>
 var path = require('path');
 var glob = require('glob');
 var async = require('async');
 var RePath = require('./lib/RePath');
+var PathStore = RePath.PathStore;
 
 var absoluteRoot = path.resolve(__dirname, process.argv[2]);
 
+var pathStore = new PathStore();
 var rePath = new RePath(absoluteRoot);
+
+function promptForNewFile(regexResult, absPathToCurrentFile, callback) {
+    rePath.utils.promptForNewFile(
+        function (newPath, callback) {
+            var absNewPath = rePath.utils.resolvePathFromFile(absPathToCurrentFile, newPath);
+
+            rePath.isFileAtPath(absNewPath, function (isFileAtPath) {
+                return callback(null, isFileAtPath, newPath, absNewPath);
+            });
+        },
+        function (err, newPath, absNewPath) {
+            if (err) {
+                return callback(err);
+            }
+
+            pathStore.memoise(absNewPath);
+
+            var transformedLine = rePath.utils.replacePath(regexResult, newPath);
+            return callback(null, transformedLine);
+        }
+    );
+}
 
 rePath.resolvePathsInFiles(
 	'**/*.js',
 	function (regexResult, absPathToCurrentFile, callback) {
 		var match = regexResult[1];
 
+        var memoisedMatches = pathStore.lookup(match);
+
 		console.log('-------------------------------------------');
 		console.log('Current File: ' + absPathToCurrentFile);
 		console.log('The following path does not exist: ' + match);
 		console.log('-------------------------------------------');
 
-        rePath.utils.prompt(
-            function (newPath, callback) {
-                var absNewPath = rePath.utils.resolvePathFromFile(absPathToCurrentFile, newPath);
-
-                rePath.isFileAtPath(absNewPath, function (isFileAtPath) {
-                    return callback(null, isFileAtPath);
-                });
-            },
-            function (err, newPath) {
-                if (err) {
-                    return callback(err);
+        if (memoisedMatches.length !== 0) {
+            rePath.utils.promptList(memoisedMatches, function (err, selectedPath) {
+                if (selectedPath === 'None') {
+                    return promptForNewFile(regexResult, absPathToCurrentFile, callback);
                 }
 
-                var transformedLine = rePath.utils.replacePath(regexResult, newPath);
+                var relativeNewPath = rePath.utils.resolvePathToFile(absPathToCurrentFile, selectedPath);
+
+                var transformedLine = rePath.utils.replacePath(regexResult, relativeNewPath);
                 return callback(null, transformedLine);
-            }
-        );
+            });
+        }
+        else {
+            promptForNewFile(regexResult, absPathToCurrentFile, callback);
+        }
+        
 	},
 	function (err, numFilesChanged, matchingFiles) {
 		if (err) throw new Error(err);
 		console.log('Number of files iterated: ' + numFilesChanged);
 
 		console.log(matchingFiles);
+
+        console.log(pathStore._store);
 	}
 );
